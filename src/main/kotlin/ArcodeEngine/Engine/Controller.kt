5230804f84967db280f5551c1e6002217b5f0083
Mason199004 <mason1990haines@gmail.com>
2020-12-09 13:18:25 -0600

    TODO: glfw joysticks because sdl2 support for java is nonexistant

--- src/main/kotlin/ArcodeEngine/Engine/Controller.kt
+++ src/main/kotlin/ArcodeEngine/Engine/Controller.kt
@@ -1,47 +1,17 @@
 package ArcodeEngine.Engine
 
-import org.libsdl.SDL.*
-import org.libsdl.SDL_Joystick
+
 
 
 class Controller
 {
     companion object
     {
-        lateinit var JoySticks: MutableList<SDL_Joystick>
+
         fun init()
         {
-            try {
-                if (SDL_Init(SDL_INIT_JOYSTICK) < 0)
-                {
-                    throw InitException("Failed to Initialize SDL")
-                }
-                else
-                {
-                    JoySticks = mutableListOf()
-                    SDL_JoystickEventState(1)
-                    for (i in 0..SDL_NumJoysticks())
-                    {
-                        JoySticks.add(SDL_Joystick.JoystickOpen(i))
-                    }
-                    var event: Event = Event()
-                    while (true)
-                    {
-                        SDL_PollEvent(event)
-                        println(JoySticks[0].getAxis(0))
 
-                    }
-                }
-            }
-            catch (e: InitException)
-            {
-                throw e
-            }
-            catch (e: Exception)
-            {
-                e.printStackTrace()
-                throw InitException("Failed to Initialize SDL")
-            }
+
 
 
 
