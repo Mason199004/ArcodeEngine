ee8fc3f18818e16dde2b7fc59644e7325c44c902
Mason199004 <mason1990haines@gmail.com>
2020-12-11 13:19:25 -0600

    b

--- src/main/kotlin/ArcodeEngine/Engine/Controller.kt
+++ src/main/kotlin/ArcodeEngine/Engine/Controller.kt
@@ -7,15 +7,55 @@ class Controller
 {
     companion object
     {
-        fun getJoystickAxes() : FloatArray
+        private fun getJoystickAxes() : FloatArray
         {
-            var arr = FloatArray(2)
-
-            if(GLFW.glfwJoystickPresent(GLFW.GLFW_JOYSTICK_1))
-                arr[0] = GLFW.glfwGetJoystickAxes(GLFW.GLFW_JOYSTICK_1)!![0]
-                arr[1] = GLFW.glfwGetJoystickAxes(GLFW.GLFW_JOYSTICK_1)!![1]
+            val arr = FloatArray(2)
+            //Joystick1 is reserved or something, always -1
+            if(GLFW.glfwJoystickPresent(GLFW.GLFW_JOYSTICK_2))
+                arr[0] = GLFW.glfwGetJoystickAxes(GLFW.GLFW_JOYSTICK_2)!![0]
+                arr[1] = GLFW.glfwGetJoystickAxes(GLFW.GLFW_JOYSTICK_2)!![1]
 
             return arr
+        }
+
+        enum class JoyState(xAxis: Int, yAxis: Int)
+        {
+            Up(0,1),
+            UpRight(-1, 1),
+            Right(-1, 0),
+            DownRight(-1, -1),
+            Down(0, -1),
+            DownLeft(1, -1),
+            Left(1, 0),
+            UpLeft(1, 1)
+
+        }
+        fun getJoystickState(Joystick: Int = 1) : JoyState
+        {
+            if (Joystick != 1 || Joystick != 2)
+            {
+                throw IllegalArgumentException("There are only 2 Joysticks connected!\nAttempted to access Joystick $Joystick")
+            }
+            val rawJoyState = getJoystickAxes()
+            when (rawJoyState[0].toInt())
+            {
+                -1 ->
+                {
+                    when (rawJoyState[1].toInt())
+                    {
+                        -1 -> return JoyState.DownRight
+                        0 -> return JoyState.Right
+                        1 -> return JoyState.UpRight
+                    }
+                }
+                0 ->
+                {
+                    when (rawJoyState[1].toInt())
+                    {
+                        -1 -> return
+                    }
+                }
+            }
         }
     }
 }
