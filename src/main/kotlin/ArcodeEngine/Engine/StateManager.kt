c4f03bb2f251d2a8b47005908d4b93c514a40eff
Richard Davidson <richarddavidson000@gmail.com>
2020-12-11 10:45:51 -0600

    removed useless check

--- src/main/kotlin/ArcodeEngine/Engine/StateManager.kt
+++ src/main/kotlin/ArcodeEngine/Engine/StateManager.kt
@@ -5,32 +5,30 @@ import java.util.*
 class StateManager {
     companion object {
         private val states: Stack<State> = Stack()
-        private lateinit var currentState: State //the entire point of using kotlin is to avoid null
+        private lateinit var currentState: State
 
         fun tickState() {
-            if (currentState != null) {
-                var lastTime = System.nanoTime()
-                val amountOfTicks = 60.0
-                val ns = 1000000000 / amountOfTicks
-                var delta = 0.0
-                var timer = System.currentTimeMillis()
-                var frames = 0
-                while (true) {
-                    val now = System.nanoTime()
-                    delta += (now - lastTime) / ns
-                    lastTime = now
-                    while (delta >= 1) {
-                        currentState.tick()
-                        delta--
-                    }
+            var lastTime = System.nanoTime()
+            val amountOfTicks = 60.0
+            val ns = 1000000000 / amountOfTicks
+            var delta = 0.0
+            var frames = 0
+            var timer = System.currentTimeMillis()
+            while (true) {
+                val now = System.nanoTime()
+                delta += (now - lastTime) / ns
+                lastTime = now
+                while (delta >= 1) {
                     currentState.tick()
-                    currentState.render()
-                    frames++
-                    if (System.currentTimeMillis() - timer > 1000) {
-                        timer += 1000
-                        println("FPS: $frames, Joystick1_State: ${Controller.getJoystickAxes()[0]} ${Controller.getJoystickAxes()[1]}")
-                        frames = 0
-                    }
+                    delta--
+                }
+                currentState.tick()
+                currentState.render()
+                frames++
+                if (System.currentTimeMillis() - timer > 1000) {
+                    timer += 1000
+                    println("FPS: $frames, Joystick1_State: ${Controller.getJoystickAxes()[0]} ${Controller.getJoystickAxes()[1]}")
+                    frames = 0
                 }
             }
         }
