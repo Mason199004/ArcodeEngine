284953047123e7e0e5b9ed5e70c510e56a350836
Mason Haines <mason1990haines@gmail.com>
2020-12-13 01:46:00 -0600

    controller stuff should be good now, also i almost made pong

--- src/main/kotlin/ArcodeEngine/Cabinet/Cabinet.kt
+++ src/main/kotlin/ArcodeEngine/Cabinet/Cabinet.kt
@@ -1,9 +1,6 @@
 package ArcodeEngine.Cabinet
 
-import ArcodeEngine.Engine.ArcodeEngine
-import ArcodeEngine.Engine.GameState
-import ArcodeEngine.Engine.StateManager
-import ArcodeEngine.Engine.Window
+import ArcodeEngine.Engine.*
 import org.lwjgl.glfw.GLFW
 import org.lwjgl.opengl.GL
 import org.lwjgl.opengl.GL11.*
@@ -12,19 +9,30 @@ import org.lwjgl.opengl.GL11.*
 
 // MASON PLS TEST THIS IN CLASS IDK IF IT ACTUALLY WORKS IT SHOULD JUST BE A RED SCREEN
 
-class Cabinet : GameState("Arcade Cabinet") {
-    private var window: Window
+class Cabinet(private var window: Window) : GameState("Arcade Cabinet") {
+
+
+
 
     init {
-        window = Window(Pair(720, 480))
+
         ArcodeEngine.submitStateChangeRequest(ArcodeEngine.StateRequest.PUSH, this)
 
     }
 
     companion object {
         @JvmStatic
+        lateinit var wind: Window
+        @JvmStatic
+        fun getWindow() : Long
+        {
+            return wind.getWindowHandle()
+        }
+
+        @JvmStatic
         fun main(args: Array<String>) {
-            Cabinet()
+            wind = Window(Pair(720, 480))
+            Cabinet(wind)
         }
     }
 
@@ -34,10 +42,60 @@ class Cabinet : GameState("Arcade Cabinet") {
         glClearColor(1f, 0f, 0f, 0f)
         StateManager.tickState()
     }
-
-    override fun tick() {}
+    var xpos = 1f
+    var ypos = 1f
+    var r1xpos = 1f
+    var r1ypos = 1f
+    var r2xpos = 1f
+    var r2ypos = 1f
+    var rectxspeed = 0f
+    var rectyspeed = 0f
+    var ticks = 0
+    override fun tick()
+    {
+        if (ticks == 1)
+        {
+            r1xpos = -0.9f
+            r2xpos = 0.9f
+            rectxspeed = -1f
+            rectyspeed = -0.5f
+        }
+        if (ticks % 10 == 0)
+        {
+            val joyState1 = Controller.getJoystickState(1)
+            val joyState2 = Controller.getJoystickState(2)
+            //r1xpos -= joyState.getX() * 0.025f
+            r1ypos += joyState1.getY() * 0.025f
+            r2ypos += joyState2.getY() * 0.025f
+        }
+        xpos += (rectxspeed * 0.0025f)
+        ypos += (rectyspeed * 0.0025f)
+        ticks++
+    }
     override fun render() {
         glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+        glColor3f(0.0f, 0.0f, 0.0f)
+        glBegin(GL_QUADS)
+
+        //rect1
+        glVertex2f(-0.025f + r1xpos, 0.2f + r1ypos)
+        glVertex2f(-0.025f + r1xpos, -0.2f + r1ypos)
+        glVertex2f(0.025f + r1xpos, -0.2f + r1ypos)
+        glVertex2f(0.025f + r1xpos, 0.2f + r1ypos)
+
+        //rect2
+        glVertex2f(-0.025f + r2xpos, 0.2f + r2ypos)
+        glVertex2f(-0.025f + r2xpos, -0.2f + r2ypos)
+        glVertex2f(0.025f + r2xpos, -0.2f + r2ypos)
+        glVertex2f(0.025f + r2xpos, 0.2f + r2ypos)
+
+        //ball
+        glVertex2f(-0.025f + xpos, 0.025f + ypos)
+        glVertex2f(-0.025f + xpos, -0.025f + ypos)
+        glVertex2f(0.025f + xpos, -0.025f + ypos)
+        glVertex2f(0.025f + xpos, 0.025f + ypos)
+
+        glEnd()
         GLFW.glfwSwapBuffers(window.getWindowHandle())
 
         GLFW.glfwPollEvents()
--- src/main/kotlin/ArcodeEngine/Engine/Controller.kt
+++ src/main/kotlin/ArcodeEngine/Engine/Controller.kt
@@ -1,25 +1,41 @@
 package ArcodeEngine.Engine
 
+import ArcodeEngine.Cabinet.Cabinet
 import org.lwjgl.glfw.GLFW
 import org.lwjgl.glfw.GLFWJoystickCallback
+import java.lang.Exception
+import java.lang.IllegalArgumentException
+import kotlin.reflect.KProperty
 
 class Controller
 {
     companion object
     {
-        private fun getJoystickAxes() : FloatArray
+        private fun getJoystickAxes(joy: Int) : FloatArray
         {
             val arr = FloatArray(2)
             //Joystick1 is reserved or something, always -1
-            if(GLFW.glfwJoystickPresent(GLFW.GLFW_JOYSTICK_2))
-                arr[0] = GLFW.glfwGetJoystickAxes(GLFW.GLFW_JOYSTICK_2)!![0]
-                arr[1] = GLFW.glfwGetJoystickAxes(GLFW.GLFW_JOYSTICK_2)!![1]
-
+            when (joy)
+            {
+                1 ->
+                {
+                    if(GLFW.glfwJoystickPresent(GLFW.GLFW_JOYSTICK_2))
+                        arr[0] = GLFW.glfwGetJoystickAxes(GLFW.GLFW_JOYSTICK_2)!![0]
+                        arr[1] = GLFW.glfwGetJoystickAxes(GLFW.GLFW_JOYSTICK_2)!![1]
+                }
+                2 ->
+                {
+                    if(GLFW.glfwJoystickPresent(GLFW.GLFW_JOYSTICK_3))
+                        arr[0] = GLFW.glfwGetJoystickAxes(GLFW.GLFW_JOYSTICK_3)!![0]
+                        arr[1] = GLFW.glfwGetJoystickAxes(GLFW.GLFW_JOYSTICK_3)!![1]
+                }
+            }
             return arr
         }
 
-        enum class JoyState(xAxis: Int, yAxis: Int)
+        enum class JoyState(val xAxis: Int,val yAxis: Int)
         {
+
             Up(0,1),
             UpRight(-1, 1),
             Right(-1, 0),
@@ -27,16 +43,46 @@ class Controller
             Down(0, -1),
             DownLeft(1, -1),
             Left(1, 0),
-            UpLeft(1, 1)
-
-        }
-        fun getJoystickState(Joystick: Int = 1) : JoyState
-        {
-            if (Joystick != 1 || Joystick != 2)
+            UpLeft(1, 1),
+            None(0,0);
+            fun getX(): Int
             {
-                throw IllegalArgumentException("There are only 2 Joysticks connected!\nAttempted to access Joystick $Joystick")
+                return xAxis
             }
-            val rawJoyState = getJoystickAxes()
+            fun getY(): Int
+            {
+                return yAxis
+            }
+        }
+
+        private fun getJoyState(Joystick: Int) : JoyState
+        {
+            var raw = FloatArray(2)
+            if (Joystick == 1)
+            {
+                if (GLFW.glfwJoystickPresent(GLFW.GLFW_JOYSTICK_2))
+                {
+                    val buf = GLFW.glfwGetJoystickAxes(GLFW.GLFW_JOYSTICK_2)
+                    raw[0] = buf?.get(0)!!
+                    raw[1] = buf[1]
+                }
+            }
+            else
+            {
+                if (GLFW.glfwJoystickPresent(GLFW.GLFW_JOYSTICK_3))
+                {
+                    val buf = GLFW.glfwGetJoystickAxes(GLFW.GLFW_JOYSTICK_3)
+                    raw[0] = buf?.get(0)!!
+                    raw[1] = buf[1]
+                }
+            }
+            return getJoyStateFromAxis(raw[0].toInt(), raw[1].toInt())
+        }
+
+        private fun getJoyStateFromAxis(xAxis: Int, yAxis: Int) : JoyState
+        {
+
+            val rawJoyState: Array<Float> = arrayOf(xAxis.toFloat(), yAxis.toFloat())
             when (rawJoyState[0].toInt())
             {
                 -1 ->
@@ -52,10 +98,86 @@ class Controller
                 {
                     when (rawJoyState[1].toInt())
                     {
-                        -1 -> return
+                        -1 -> return JoyState.Down
+                        0 -> return JoyState.None
+                        1 -> return JoyState.Up
+                    }
+                }
+                1 ->
+                {
+                    when (rawJoyState[1].toInt())
+                    {
+                        -1 -> return JoyState.DownLeft
+                        0 -> return JoyState.Left
+                        1 -> return JoyState.UpLeft
                     }
                 }
             }
+            throw RuntimeException("Joystick returned an invalid state!")
+        }
+
+        fun getJoystickState(Joystick: Int) : JoyState
+        {
+            val state = getJoyState(Joystick)
+            if (state == JoyState.None)
+            {
+                when (Joystick)
+                {
+                    1 ->
+                    {
+                        val w = GLFW.glfwGetKey(Cabinet.getWindow(), GLFW.GLFW_KEY_W)
+                        val a = GLFW.glfwGetKey(Cabinet.getWindow(), GLFW.GLFW_KEY_A)
+                        val s = GLFW.glfwGetKey(Cabinet.getWindow(), GLFW.GLFW_KEY_S)
+                        val d = GLFW.glfwGetKey(Cabinet.getWindow(), GLFW.GLFW_KEY_D)
+                        var xAxis = 0
+                        var yAxis = 0
+                        if (w == GLFW.GLFW_PRESS)
+                        {
+                            yAxis++
+                        }
+                        if (s == GLFW.GLFW_PRESS)
+                        {
+                            yAxis--
+                        }
+                        if (a == GLFW.GLFW_PRESS)
+                        {
+                            xAxis++
+                        }
+                        if (d == GLFW.GLFW_PRESS)
+                        {
+                            xAxis--
+                        }
+                        return getJoyStateFromAxis(xAxis, yAxis)
+                    }
+                    2 ->
+                    {
+                        val w = GLFW.glfwGetKey(Cabinet.getWindow(), GLFW.GLFW_KEY_UP)
+                        val a = GLFW.glfwGetKey(Cabinet.getWindow(), GLFW.GLFW_KEY_LEFT)
+                        val s = GLFW.glfwGetKey(Cabinet.getWindow(), GLFW.GLFW_KEY_DOWN)
+                        val d = GLFW.glfwGetKey(Cabinet.getWindow(), GLFW.GLFW_KEY_RIGHT)
+                        var xAxis = 0
+                        var yAxis = 0
+                        if (w == GLFW.GLFW_PRESS)
+                        {
+                            yAxis++
+                        }
+                        if (s == GLFW.GLFW_PRESS)
+                        {
+                            yAxis--
+                        }
+                        if (a == GLFW.GLFW_PRESS)
+                        {
+                            xAxis++
+                        }
+                        if (d == GLFW.GLFW_PRESS)
+                        {
+                            xAxis--
+                        }
+                        return getJoyStateFromAxis(xAxis, yAxis)
+                    }
+                }
+            }
+            throw IllegalArgumentException("There are only 2 Joysticks connected!\nAttempted to read state of Joystick: $Joystick")
         }
     }
 }
