b5ae5d37a23d2d979f07794125c12cb857eb0718
ErrorOnUsername <richarddavidson000@gmail.com>
2021-02-08 19:42:20 -0600

    TextRenderer: text rendering ready. Just need to add the font map and make sure everything works.

--- src/main/kotlin/ArcodeEngine/Engine/Text/TextRenderer.kt
+++ src/main/kotlin/ArcodeEngine/Engine/Text/TextRenderer.kt
@@ -1,6 +1,9 @@
 package ArcodeEngine.Engine.Text
 
+import ArcodeEngine.Engine.ArcodeEngine
+import ArcodeEngine.Engine.GFX.Renderer
 import ArcodeEngine.Engine.Geometry.Rectangle
+import ArcodeEngine.Engine.Window
 import org.joml.Vector2f
 
 class TextRenderer {
@@ -8,30 +11,85 @@ class TextRenderer {
         private var cursorPosition: Vector2f = Vector2f()
         private var chars: ArrayList<Rectangle> = ArrayList()
 
-        fun DrawString(msg: String) {
+        private val FONT_MAP_ID: Int = ArcodeEngine.RegisterTexture("res/arcade-font.png")
 
+        private const val NUMBER_OFFSET: Int = 16
+        private const val CHAR_WIDTH: Int = 2
+        private const val CHAR_HEIGHT: Int = 3
+
+        fun DrawString(window: Window, x: Float, y: Float, msg: String, textScale: Float) {
+            for(c in msg)
+                DrawChar(window, x, y, c, textScale)
         }
 
-        fun DrawChar(char: Char) {
+        fun DrawChar(window: Window, x: Float, y:Float, char: Char, textScale: Float) {
+            if(char == ' ') {
+                cursorPosition.x += CHAR_WIDTH * textScale
+                return
+            }
 
+            val isAlpha: Int = IsAlpha(char)
+            val isNumber: Int = IsNumber(char)
+
+            val texCoords = if(isAlpha != -1)
+                CalculateTextureCoords(char, isAlpha)
+            else if(isNumber != -1)
+                CalculateTextureCoords(char, isNumber)
+            else
+                CalculateTextureCoords(char, 0x420beef)
+
+            val rect: Rectangle = Rectangle(x + cursorPosition.x, y + cursorPosition.y, CHAR_WIDTH * textScale, CHAR_HEIGHT * textScale, texCoords)
+            Renderer.DrawTexturedRect(window, rect, FONT_MAP_ID)
+
+            chars.add(rect)
+            cursorPosition.x += CHAR_WIDTH * textScale
         }
 
-        private fun IsAlpha(c: Char) {}
-        private fun IsNumber(c: Char) {}
+        private fun IsAlpha(c: Char): Int {
+            if(c in 'A'..'Z')
+                return (c - 0x41).toInt()
+            return -1
+        }
+        private fun IsNumber(c: Char): Int {
+            if(c in '0'..'9')
+                return (c - 0x30).toInt()
+            return -1
+        }
 
         private fun CalculateTextureCoords(char: Char, idx: Int) : FloatArray {
+            var row: Int = idx / 16
+            var col: Int = idx % 16
+
             when(char) {
-                else -> {
-                    System.err.println("The character you entered: $char is not supported")
-                    /* displays asterisk if an unsupported character is passed */
-                    return floatArrayOf(
-                        12f / 16, 2f / 3,
-                        13f / 16, 2f / 3,
-                        13f / 16, 1f / 3,
-                        12f / 16, 1f / 3
+                'A' -> {
+                    floatArrayOf(
+                        col/16f,   (1 - row/3f),
+                        col+1/16f, (1 - row/3f),
+                        col+1/16f, (1 - ((row + 1)/3f)),
+                        col/16f,   (1 - ((row + 1) / 3f))
+                    )
+                }
+                '0' -> {
+                    row = (idx+NUMBER_OFFSET) / 16
+                    col = (idx+NUMBER_OFFSET) % 16
+
+                    floatArrayOf(
+                        col/16f,   (1 - row/3f),
+                        col+1/16f, (1 - row/3f),
+                        col+1/16f, (1 - ((row + 1)/3f)),
+                        col/16f,   (1 - ((row + 1) / 3f))
                     )
                 }
             }
+
+            System.err.println("The character you entered: $char is not supported")
+            /* displays asterisk if an unsupported character is passed */
+            return floatArrayOf(
+                    12f / 16, 2f / 3,
+                    13f / 16, 2f / 3,
+                    13f / 16, 1f / 3,
+                    12f / 16, 1f / 3
+            )
         }
     }
 }
