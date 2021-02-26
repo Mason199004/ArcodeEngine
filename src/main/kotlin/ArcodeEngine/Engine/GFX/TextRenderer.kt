8d80cbd3e1bf44f4cf60f20ef7f06df06e4c9275
ErrorOnUsername <richarddavidson000@gmail.com>
2021-02-26 14:22:34 -0600

    TextRenderer: made the draw method JvmStatic so you can use it in Java.

--- src/main/kotlin/ArcodeEngine/Engine/GFX/TextRenderer.kt
+++ src/main/kotlin/ArcodeEngine/Engine/GFX/TextRenderer.kt
@@ -7,12 +7,14 @@ import ArcodeEngine.Engine.Window
 class TextRenderer {
     companion object {
         var fontMapID = ArcodeEngine.RegisterTexture("src/main/kotlin/ArcodeEngine/Engine/res/arcade-font.png")
+
+        @JvmStatic
         fun DrawString(window: Window, text: Text) {
             for(r in text.chars)
                 Renderer.DrawTexturedRect(window, r, fontMapID)
         }
 
-        fun DrawString(window: Window, text: Text, fontID: Int) {
+        private fun DrawString(window: Window, text: Text, fontID: Int) {
             for(r in text.chars) {
                 Renderer.DrawTexturedRect(window, r, fontID)
             }
