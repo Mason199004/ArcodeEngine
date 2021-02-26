cafea9243ae365ec5f3cb849390b0ff670203ed5
ErrorOnUsername <richarddavidson000@gmail.com>
2021-02-26 12:00:06 -0600

    Renderer: replaced direct references to mesh with getters

--- src/main/kotlin/ArcodeEngine/Engine/GFX/Renderer.kt
+++ src/main/kotlin/ArcodeEngine/Engine/GFX/Renderer.kt
@@ -20,9 +20,9 @@ class Renderer {
             ArcodeEngine.ColoredShader.LoadVector3f("inColor", color)
             ArcodeEngine.ColoredShader.LoadMatrix4f("mvMatrix", rectangle.GetTransformMatrix().mul(window.GetCamera().GetViewMatrix()))
             ArcodeEngine.ColoredShader.LoadMatrix4f("projMatrix", window.GetCamera().GetProjectionMatrix())
-            OpenGL.GLBindVertexArray(rectangle.mesh.vaoID);
+            OpenGL.GLBindVertexArray(rectangle.GetMesh().vaoID);
             OpenGL.GLEnableVertexAttribArray(0);
-            OpenGL.GLDrawElements(GL_TRIANGLES, rectangle.mesh.vertices.size, GL_UNSIGNED_INT, 0)
+            OpenGL.GLDrawElements(GL_TRIANGLES, rectangle.GetMesh().vertices.size, GL_UNSIGNED_INT, 0)
             OpenGL.GLDisableVertexAttribArray(0);
             OpenGL.GLBindVertexArray(0)
             ArcodeEngine.ColoredShader.Unbind()
@@ -33,12 +33,12 @@ class Renderer {
             ArcodeEngine.TexturedShader.Bind()
             ArcodeEngine.TexturedShader.LoadMatrix4f("mvMatrix", rectangle.GetTransformMatrix())
             ArcodeEngine.TexturedShader.LoadMatrix4f("projMatrix", window.GetCamera().GetProjectionMatrix())
-            OpenGL.GLBindVertexArray(rectangle.mesh.vaoID)
+            OpenGL.GLBindVertexArray(rectangle.GetMesh().vaoID)
             OpenGL.GLEnableVertexAttribArray(0)
             OpenGL.GLEnableVertexAttribArray(1)
             OpenGL.GLActiveTexture(GL_TEXTURE0)
             OpenGL.GLBindTexture(GL_TEXTURE_2D, textureID)
-            OpenGL.GLDrawElements(GL_TRIANGLES, rectangle.mesh.vertices.size, GL_UNSIGNED_INT, 0)
+            OpenGL.GLDrawElements(GL_TRIANGLES, rectangle.GetMesh().vertices.size, GL_UNSIGNED_INT, 0)
             OpenGL.GLDisableVertexAttribArray(1)
             OpenGL.GLDisableVertexAttribArray(0)
             OpenGL.GLBindVertexArray(0)
