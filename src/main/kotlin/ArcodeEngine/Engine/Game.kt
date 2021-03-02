f874db19a8eba2ac33e106ad15ab1c6108697e30
ErrorOnUsername <richarddavidson000@gmail.com>
2021-03-01 23:32:54 -0600

    Game: Added TryExit default function to GameState base class.

--- src/main/kotlin/ArcodeEngine/Engine/Game.kt
+++ src/main/kotlin/ArcodeEngine/Engine/Game.kt
@@ -1,7 +1,22 @@
 package ArcodeEngine.Engine
 
+import ArcodeEngine.Engine.*
+import org.lwjgl.glfw.GLFW
+
 abstract class GameState(val name: String, var window: Window) {
     abstract fun Init()
     abstract fun Tick()
     abstract fun Render()
+
+    fun TryExit() {
+        var esc = GLFW.glfwGetKey(window.GetWindowHandle(), GLFW.GLFW_KEY_ESCAPE)
+        if (esc == GLFW.GLFW_PRESS) {
+            while (esc != GLFW.GLFW_RELEASE) {
+                GLFW.glfwPollEvents()
+                esc = GLFW.glfwGetKey(window.GetWindowHandle(), GLFW.GLFW_KEY_ESCAPE)
+            }
+            StateManager.PopState()
+            return
+        }
+    }
 }
