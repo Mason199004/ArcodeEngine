d93e8e0aebcfb802179e724113f473040870b178
Mason199004 <mason1990haines@gmail.com>
2021-04-30 13:17:24 -0500

    please

--- src/main/kotlin/ArcodeEngine/Engine/GFX/Shader/coloredFrag.glsl
+++ src/main/kotlin/ArcodeEngine/Engine/GFX/Shader/coloredFrag.glsl
@@ -5,5 +5,5 @@ in mediump vec3 color;
 out mediump vec4 out_Color;
 
 void main(void) {
-    out_Color = vec4(color, 0);
+    out_Color = vec4(color, 1.0);
 }
--- src/main/kotlin/ArcodeEngine/Engine/GFX/Shader/coloredVert.glsl
+++ src/main/kotlin/ArcodeEngine/Engine/GFX/Shader/coloredVert.glsl
@@ -1,15 +1,14 @@
-#version 300 es 
+#version 300 es
 
 in mediump vec3 position;
 
 out mediump vec3 color;
 
-uniform mat4 mvMatrix;
-uniform mat4 projMatrix;
+uniform mat4 mvpMatrix;
 uniform vec3 inColor;
 
 void main(void) {
     // proj * view * model(transformation)
-    gl_Position = projMatrix * mvMatrix * vec4(position, 1.0);
+    gl_Position = mvpMatrix * vec4(position, 1.0);
     color = inColor;
-}
+} 
